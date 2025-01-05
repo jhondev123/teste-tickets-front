6edeea11e579566a1407a5320f7f429723560dad
jhondev123 <jhonattan.curtarelli@outlook.com>
2025-01-04 23:42:55 -0300

    feat: creating search employees

--- src/app/employee/page.tsx
+++ src/app/employee/page.tsx
@@ -5,6 +5,7 @@ import axios from '../../utils/axios';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faEdit} from '@fortawesome/free-solid-svg-icons';
 import Link from "next/link";
+
 type Ticket = {
     id: number;
     quantity: number;
@@ -31,11 +32,13 @@ type Employee = {
 const EmployeePage: React.FC = () => {
     const [employees, setEmployees] = useState<Employee[]>([]);
     const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [debounceTimeout, setDebounceTimeout] = useState<NodeJS.Timeout | number | null>(null);
 
     useEffect(() => {
         const fetchEmployees = async () => {
             try {
-                const response = await axios.get('employees');
+                const response = await axios.get('employee/search');
                 setEmployees(response.data.data);
                 setLoading(false);
             } catch (error) {
@@ -46,6 +49,30 @@ const EmployeePage: React.FC = () => {
         fetchEmployees();
     }, []);
 
+    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setSearchTerm(value);
+
+        if (debounceTimeout) {
+            clearTimeout(debounceTimeout as NodeJS.Timeout);
+        }
+
+        const timeout = setTimeout(() => {
+            fetchEmployeeSearch(value);
+        }, 1000);
+
+        setDebounceTimeout(timeout);
+    };
+
+    const fetchEmployeeSearch = async (slug: string) => {
+        try {
+            const response = await axios.get('employee/search', {params: {slug}});
+            setEmployees(response.data.data);
+        } catch (error) {
+            console.error('Erro ao fazer pesquisa', error);
+        }
+    };
+
     if (loading) {
         return <div>Carregando...</div>;
     }
@@ -53,13 +80,24 @@ const EmployeePage: React.FC = () => {
     return (
         <div className="p-8">
             <h1 className="text-2xl font-bold mb-6">Funcionários</h1>
-            <div className="flex justify-end mb-6">
-                <Link
-                    className="bg-blue-500 text-white rounded-full p-3 shadow-lg hover:bg-blue-600 transition"
-                    href="/employee/store"
-                >
-                    Adicionar Funcionário
-                </Link>
+            <div className="flex justify-between items-center mb-2">
+                <div className="w-80">
+                    <input
+                        type="text"
+                        className="w-full border border-gray-200 rounded-lg p-3 shadow-lg focus:ring focus:ring-blue-300"
+                        placeholder="Pesquisar"
+                        value={searchTerm}
+                        onChange={handleSearchChange} // Attach onChange to handle input change
+                    />
+                </div>
+                <div>
+                    <Link
+                        className="bg-blue-500 text-white rounded-full px-6 py-3 shadow-lg hover:bg-blue-600 transition focus:ring focus:ring-blue-300"
+                        href="/employee/store"
+                    >
+                        Adicionar Funcionário
+                    </Link>
+                </div>
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {employees.map((employee) => (
@@ -74,27 +112,7 @@ const EmployeePage: React.FC = () => {
                             <div className="mt-4">
                                 <h3 className="font-semibold text-lg">Tickets</h3>
                                 <p>Quantidade Total: {employee.tickets.total_quantity}</p>
-                                <p>Tickets Ativos: {employee.tickets.total_actives}</p>
-                                <p>Tickets Inativos: {employee.tickets.total_inactives}</p>
-                                <div className="mt-2">
-                                    <h4 className="text-sm font-semibold">Detalhes dos Tickets</h4>
-                                    {employee.tickets.tickets.map((ticket) => (
-                                        <div
-                                            key={ticket.id}
-                                            className="flex justify-between items-center border-b py-2"
-                                        >
-                                            <span>ID do Ticket: {ticket.id}</span>
-                                            <span>Quantidade: {ticket.quantity}</span>
-                                            <span
-                                                className={`text-sm ${
-                                                    ticket.situation === 'A' ? 'text-green-500' : 'text-red-500'
-                                                }`}
-                                            >
-                                                {ticket.situation === 'A' ? 'Ativo' : 'Inativo'}
-                                            </span>
-                                        </div>
-                                    ))}
-                                </div>
+
                                 <div className="mt-2">
                                     <p className="text-sm text-gray-400">Criado em: {employee.created_at}</p>
                                     <p className="text-sm text-gray-400">Atualizado em: {employee.updated_at}</p>
